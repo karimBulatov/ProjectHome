6bdf8fa7f6d82ad5ddbf8ef86d854df1365784a6
Карим <kbulatov01@gmail.com>
2025-02-20 18:16:00 +0300

    123

---
+++ src/main/java/com/yourcompany/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.yourcompany;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Servlet!</h1>");
+    }
+}
+
